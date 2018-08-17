f92ba8a723fafacd42a8c1a69be4ed38a69a595f
jacky <jacky.cheng@yeahmobi.com>
2018-08-17 17:14:47 +0800

    update json property

--- src/main/java/com/huobi/api/client/domain/DepositWithdraw.java
+++ src/main/java/com/huobi/api/client/domain/DepositWithdraw.java
@@ -1,5 +1,6 @@
 package com.huobi.api.client.domain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.huobi.api.client.domain.enums.DepositWithdrawState;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,12 +14,16 @@ public class DepositWithdraw {
     private Long id;//true	long
     private String type;//true	long	类型	'deposit' 'withdraw'
     private String currency;//true	string	币种
-    private String tx_hash;//true	string	交易哈希
+    @JsonProperty("tx-hash")
+    private String txHash;//true	string	交易哈希
     private String amount;//true	long	个数
     private String address;//true	string	地址
-    private String address_tag;//true	string	地址标签
+    @JsonProperty("address-tag")
+    private String addressTag;//true	string	地址标签
     private String fee;//true	long	手续费
     private DepositWithdrawState state;//true	string	状态	状态参见下表
-    private long created_at;//true	long	发起时间
-    private long updated_at;//
+    @JsonProperty("created-at")
+    private long createdAt;//true	long	发起时间
+    @JsonProperty("updated-at")
+    private long updatedAt;
 }
