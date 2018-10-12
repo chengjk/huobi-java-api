ff81a5657ba5c6b1dabadc48198a4a513c94cd0f
jacky <jacky.cheng@yeahmobi.com>
2018-10-12 11:15:58 +0800

    refactor huobiException

--- src/main/java/com/huobi/api/client/security/HuobiSigner.java
+++ src/main/java/com/huobi/api/client/security/HuobiSigner.java
@@ -2,15 +2,12 @@ package com.huobi.api.client.security;
 
 import com.huobi.api.client.constant.HuobiConsts;
 
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.NoSuchAlgorithmException;
 import java.time.Instant;
-import java.util.*;
+import java.util.Map;
+import java.util.StringJoiner;
+import java.util.TreeSet;
 
 /**
  * created by jacky. 2018/9/27 2:34 PM
@@ -74,49 +71,5 @@ public class HuobiSigner {
     public String gmtNow() {
         return Instant.now().atZone(HuobiConsts.ZONE_GMT).format(HuobiConsts.DT_FORMAT);
     }
-
-
-
-    //////--------------
-    public static void main(String[] args) {
-        String secretKey = "c84dab82-12dc9c5f-47d4d371-b17df";
-        String payload = "GET\n" +
-                "api.huobi.pro:443\n" +
-                "/ws/v1\n" +
-                "AccessKeyId=6ab18816-7510b23c-70b60749-71c31&SignatureMethod=HmacSHA256&SignatureVersion=2&Timestamp=2018-09-27T08%3A30%3A30";
-        String sign = HmacSHA256Signer.sign(payload, secretKey);
-
-
-        String signatureMethodValue = "HmacSHA256";
-        Mac hmacSha256;
-        try {
-            hmacSha256 = Mac.getInstance(signatureMethodValue);
-            SecretKeySpec secKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), signatureMethodValue);
-            hmacSha256.init(secKey);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("No such algorithm: " + e.getMessage());
-        } catch (InvalidKeyException e) {
-            throw new RuntimeException("Invalid key: " + e.getMessage());
-        }
-        byte[] hash = hmacSha256.doFinal(payload.getBytes(StandardCharsets.UTF_8));
-
-        //获取签名，并进行Base64编码
-        String actualSign = Base64.getEncoder().encodeToString(hash);
-
-
-        HashMap<String, String> map = new HashMap<>();
-        String sign1 = new HuobiSigner("6ab18816-7510b23c-70b60749-71c31", secretKey).sign("GET", "/ws/v1", map, "2018-09-27T08:30:30");
-
-        if (sign.equals(sign1)) {
-            System.out.println("asdfa");
-        }
-
-
-        if (actualSign.equals(sign)) {
-            System.out.println("Sss");
-        }
-        System.out.println(sign);
-    }
-
 
 }
