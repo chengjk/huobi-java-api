292521060edd0f9fee9a0ad2b51087f241aa4a97
jacky <jacky.cheng@yeahmobi.com>
2019-07-05 10:37:39 +0800

    update symbol model. add fields

--- src/main/java/com/huobi/api/client/domain/Symbol.java
+++ src/main/java/com/huobi/api/client/domain/Symbol.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 /**
  * created by jacky. 2018/7/21 2:59 PM
  */
@@ -21,4 +23,16 @@ public class Symbol {
     private Integer amountPrecision;
     @JsonProperty("symbol-partition")
     private String symbolPartition;
+    private String state;
+    @JsonProperty("value-precision")
+    private Integer valuePrecision;
+    @JsonProperty("min-order-amt")
+    private BigDecimal minOrderAmt;
+    @JsonProperty("max-order-amt")
+    private BigDecimal maxOrderAmt;
+    //最小下单金额 （下单金额指当订单类型为限价单时，下单接口传入的(amount * price)。当订单类型为buy-market时，下单接口传的'amount'）
+    @JsonProperty("min-order-value")
+    private BigDecimal minOrderValue;
+    @JsonProperty("leverage-ratio")
+    private Integer leverageRatio;
 }
